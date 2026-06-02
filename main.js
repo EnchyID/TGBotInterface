49751a65334b01d94202a6feabca9a9100c56714
Enchy <91828204+EnchyID@users.noreply.github.com>
2026-06-02 10:50:40 +0800

    Initialize Telegraf bot with start command

---
+++ main.js
@@ -0,0 +1,14 @@
+const { Telegraf } = require("telegraf")
+const cfg = require("config.json")
+const bot = new Telegraf(cfg.APITOKEN)
+
+bot.on("text", ctx => {
+  let text = ctx.message.text
+  if(text == "/start"){
+    ctx.reply(lang["eng"].startMessage)
+  }else{
+    ctx.reply(lang["eng"].existsCommand)
+  }
+})
+
+bot.launch()
